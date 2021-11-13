f4fd2936c0184179abd08655a3784e57445ec40e
Modestas Skirius <modestas.skirius@gmail.com>
2021-11-13 11:43:11 +0200

    add sessionController

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+
+    public function destroy()
+    {
+        auth()->logout();
+
+        return redirect('/')->with('success', 'Goodbye!');
+    }
+}
