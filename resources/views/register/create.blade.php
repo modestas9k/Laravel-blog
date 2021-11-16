96341c15b962f9bfa99fc037154ba920c6c36e2b
Modestas Skirius <modestas.skirius@gmail.com>
2021-11-16 11:22:00 +0200

    formatting

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -5,58 +5,58 @@
             <form method="POST" action="/register" class="mt-10">
                 @csrf
                 <div class="mb-6">
-                    <label 
-                        class="upercase block mb-2 font-bold text-xs text-gray-700"
+                    <label
+                        class="uppercase block mb-2 font-bold text-xs text-gray-700"
                         for="name">
                         Name
-                     </label>
-                    <input class="border border-gray-400 p-2 w-full" 
-                        type="text" 
-                        name="name" 
-                        id="name" 
-                        value="{{ old('name') }}"
-                        required>
-                        @error('name')
-                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                        @enderror
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="text"
+                           name="name"
+                           id="name"
+                           value="{{ old('name') }}"
+                           required>
+                    @error('name')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
-                    <label 
+                    <label
                         class="upercase block mb-2 font-bold text-xs text-gray-700"
                         for="username">
                         Username
-                     </label>
-                    <input class="border border-gray-400 p-2 w-full" 
-                        type="text" 
-                        name="username" 
-                        id="username" 
-                        value="{{ old('username') }}"
-                        required>
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="text"
+                           name="username"
+                           id="username"
+                           value="{{ old('username') }}"
+                           required>
                 </div>
                 <div class="mb-6">
-                    <label 
+                    <label
                         class="upercase block mb-2 font-bold text-xs text-gray-700"
                         for="email">
                         Email
-                     </label>
-                    <input class="border border-gray-400 p-2 w-full" 
-                        type="email" 
-                        name="email" 
-                        id="email" 
-                        value="{{ old('email') }}"
-                        required>
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="email"
+                           name="email"
+                           id="email"
+                           value="{{ old('email') }}"
+                           required>
                 </div>
                 <div class="mb-6">
-                    <label 
+                    <label
                         class="upercase block mb-2 font-bold text-xs text-gray-700"
                         for="password">
                         Password
-                     </label>
-                    <input class="border border-gray-400 p-2 w-full" 
-                        type="password" 
-                        name="password" 
-                        id="password" 
-                        required>
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="password"
+                           name="password"
+                           id="password"
+                           required>
                 </div>
                 <div class="mb-6">
                     <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
