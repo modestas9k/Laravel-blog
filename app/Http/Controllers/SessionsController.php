c9f25113524715adb1716f16c60968b8ba058f9e
Modestas Skirius <modestas.skirius@gmail.com>
2021-11-16 11:35:07 +0200

    add login function

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
     public function create()
@@ -9,6 +11,27 @@ class SessionsController extends Controller
         return view('sessions.create');
     }
 
+    public function store()
+    {
+        // validate the request
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+
+            return redirect('/')->with('success', 'Welcome back!');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+//        return back()
+//            ->withInput()
+//            ->withErrors(['email' => 'Your provided credentials could not be verified.']);
+    }
 
     public function destroy()
     {
@@ -17,3 +40,5 @@ class SessionsController extends Controller
         return redirect('/')->with('success', 'Goodbye!');
     }
 }
+
+
