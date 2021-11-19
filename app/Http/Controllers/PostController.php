fb8ae1beef0f0d9312d837fb25927f343123b0cf
Modestas Skirius <modestas.skirius@gmail.com>
2021-11-19 10:31:10 +0200

    validate and store thumbnails

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,23 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
-use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
 
 class PostController extends Controller
 {
-    public function index() {
+    public function index()
+    {
 
         return view('posts.index', [
             'posts' => Post::latest()->filter(
                 request(['search', 'category', 'author'])
-                )->paginate(3)->withQueryString()
+            )->paginate(3)->withQueryString()
         ]);
     }
-    public function show(Post $post) {
+
+    public function show(Post $post)
+    {
         return view('posts.show', [
             'post' => $post
         ]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'title' => 'required',
+            'thumbnail' => 'required|image',
+            'slug' => ['required', Rule::unique('posts', 'slug')],
+            'excerpt' => 'required',
+            'body' => 'required',
+            'category_id' => ['required', Rule::exists('categories', 'id')]
+        ]);
+        $attributes['user_id'] = auth()->id();
+        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails', 'public');
+
+        Post::create($attributes);
+
+        return redirect('/');
     }
 }
--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -4,13 +4,13 @@
     {{ $attributes->merge(['class' => "transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl" ])}}>
     <div class="py-6 px-5">
         <div>
-            <img src="/images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
+            <img src="{{ asset('storage/' . $post->thumbnail ) }}" alt="Blog Post illustration" class="rounded-xl">
         </div>
 
         <div class="mt-8 flex flex-col justify-between">
             <header>
                 <div class="space-x-2">
-                    <x-category-button :category="$post->category" />
+                    <x-category-button :category="$post->category"/>
                 </div>
 
                 <div class="mt-4">
@@ -45,7 +45,7 @@
 
                 <div>
                     <a href="/posts/{{ $post->slug }}"
-                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">
+                       class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">
                         Read More
                     </a>
                 </div>
